18bd63077cb3979518e9157f8d6fb02f52c8b23b
Sjors Smit <53516987+Shorts1999@users.noreply.github.com>
2021-10-28 08:34:46 +0200

    Match partition offset to other Twomes upload scripts

--- CustomUpload.bat
+++ CustomUpload.bat
@@ -14,12 +14,12 @@ goto manual
 
 :autodetect
     echo Uploading to device...
-    %1 %2 --chip esp32 --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 .\binaries\bootloader_qio_80m.bin 0x8000 .\binaries\partitions.bin 0xe000 .\binaries\boot_app0.bin 0x10000 .\binaries\firmware.bin  
+    %1 %2 --chip esp32 --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 .\binaries\bootloader_qio_80m.bin 0x9000 .\binaries\partitions.bin 0xe000 .\binaries\boot_app0.bin 0x10000 .\binaries\firmware.bin  
     goto end
 
 :manual
     echo Uploading to device on %3
-    %1 %2 --chip esp32 --port %3 --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 .\binaries\bootloader_qio_80m.bin 0x8000 .\binaries\partitions.bin 0xe000 .\binaries\boot_app0.bin 0x10000 .\binaries\firmware.bin
+    %1 %2 --chip esp32 --port %3 --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 .\binaries\bootloader_qio_80m.bin 0x9000 .\binaries\partitions.bin 0xe000 .\binaries\boot_app0.bin 0x10000 .\binaries\firmware.bin
     goto end
 
 :end
